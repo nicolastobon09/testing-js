46a41d7969941d164bb1cef7196af559cde7073e
Nicolás Alvarez <nicolastobon009@Nicolas-Alvarezs-MacBook-Pro.local>
2025-01-18 15:51:43 -0500

    add files and config

---
+++ demos/src/02-math.ts
@@ -0,0 +1,14 @@
+export function sum(a: number, b:number): number {
+  return a + b
+}
+
+export function multiplay(a: number, b:number): number {
+  return a * b
+}
+
+export function divide(a: number, b:number): number | null {
+  if (b === 0) {
+    return null;
+  }
+  return a / b
+}
